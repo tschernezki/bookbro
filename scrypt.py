fb8ddb3d1b55d61fda00eacdf564e65472b3937e
Artem Tschernezki <artem@post.cz>
2023-11-19 17:03:41 +0200

    Update scrypt.py

--- scrypt.py
+++ scrypt.py
@@ -25,14 +25,17 @@ def time_until_next_message(hour):
 
 # Асинхронная функция для отправки сообщения в Telegram
 async def send_message_to_telegram_channel(text, bot_token, channel_id):
-    bot = telegram.Bot(token=bot_token)
-    await bot.send_message(chat_id=channel_id, text=text)
+    try:
+        bot = telegram.Bot(token=bot_token)
+        await bot.send_message(chat_id=channel_id, text=text)
+    except Exception as e:
+        logging.error(f"Ошибка при отправке сообщения: {e}")
 
 # Функция для генерации краткого содержания
 def generate_summary(text):
     openai.api_key = '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'
     response = openai.ChatCompletion.create(
-        model="gpt-4-1106-preview",
+        model="gpt-4",
         messages=[
             {"role": "system", "content": "Вы публицист, экономист и историк, который читает книгу и пишет блог в повествовательном стиле. Ваша задача - проанализировать и пересказать в доступной манере, предоставленный текст из книги. Начиная текст, придумайте фразу вроде 'Анализируя прочитанное, я заметил ...' или 'Продолжая читать книгу, обнаружил интересные мысли...' и в этой фразе упоминайте главу, которую читаете - ее номер или название и упоминайте автора и название книги. Уложитесь в 250 слов."},
             {"role": "user", "content": text}
@@ -44,21 +47,25 @@ def generate_summary(text):
 
 # Основная функция
 async def process_book(file_path, bot_token, channel_id):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        book_text = file.read()
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            book_text = file.read()
 
-    chapters = split_book_into_parts(book_text)
+        chapters = split_book_into_parts(book_text)
 
-    for chapter_number, chapter_text in enumerate(chapters, start=1):
-        summary = generate_summary(f"Глава {chapter_number}\n{chapter_text}")
-        await send_message_to_telegram_channel(summary, bot_token, channel_id)
-        if chapter_number % 2 == 0:
-            await asyncio.sleep(time_until_next_message(15))  # Отправка в 15:00 UTC
-        else:
-            await asyncio.sleep(time_until_next_message(9))   # Отправка в 09:00 UTC
+        for chapter_number, chapter_text in enumerate(chapters, start=1):
+            logging.info(f"Обработка главы {chapter_number}")
+            summary = generate_summary(f"Глава {chapter_number}\n{chapter_text}")
+            await send_message_to_telegram_channel(summary, bot_token, channel_id)
+            if chapter_number % 2 == 0:
+                await asyncio.sleep(time_until_next_message(16))  # Отправка в 16:00 UTC
+            else:
+                await asyncio.sleep(time_until_next_message(9))   # Отправка в 09:00 UTC
+    except Exception as e:
+        logging.error(f"Произошла ошибка: {e}")
 
 # Пример использования
-file_path = "book-bot.txt"
+file_path = "book-bot.txt"  # Используйте относительный путь
 bot_token = '6786746440:AAF2yGdkXhWdnPRzkYZDz1-gweckuTUp-ss'
 channel_id = '@rheniumbooks'
 
