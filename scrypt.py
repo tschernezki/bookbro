4c4e49f265fdb45df08a80ca5375fa70549bd01e
Artem Tschernezki <artem@post.cz>
2023-11-28 22:04:25 +0200

    Update scrypt.py

--- scrypt.py
+++ scrypt.py
@@ -40,10 +40,6 @@ def time_until_next_message(hour, minute):
 
 # Асинхронная функция для отправки сообщения в Telegram
 async def send_message_to_telegram_channel(text, bot_token, channel_id):
-    if not text.strip():
-        logging.warning("Текст сообщения пуст. Сообщение не будет отправлено.")
-        return
-
     try:
         bot = telegram.Bot(token=bot_token)
         await bot.send_message(chat_id=channel_id, text=text)
@@ -56,48 +52,36 @@ def generate_summary(text):
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "Вы публицист, экономист, и убежденный правый либертарианец-капиталист, который читает книги и пишет блог в повествовательном стиле. Ваша задача - проанализировать и пересказать в доступной манере, предоставленный текст из книги, сравнивая описываемое в каждой главе, с экономическими и социальными реалиями 21 века. Начиная, придумайте фразу вроде 'Анализируя прочитанное, я заметил ...' или 'Читая книгу, обнаружил интересные мысли...' и во фразе упоминайте номер главы, которую прочли, ее название и упоминайте автора и название книги. Уложитесь в 300 слов. Пишите на украинском языке, в разговорном но не фамильярном стиле."},
+            {"role": "system", "content": "Вы публицист, экономист, и убежденный либертарианец-капиталист, который читает книги и пишет блог в повествовательном стиле. Ваша задача - проанализировать и пересказать в доступной манере, предоставленный текст из книги, сравнивая описываемое в каждой главе, с экономическими и социальными реалиями 21 века. Начиная, придумайте фразу вроде 'Анализируя прочитанное, я заметил ...' или 'Читая книгу, обнаружил интересные мысли...' и во фразе упоминайте номер главы, которую прочли, ее название и упоминайте автора и название книги. Уложитесь в 300 слов. Пишите на украинском языке, в разговорном но не фамильярном стиле."},
             {"role": "user", "content": text}
         ],
-        max_tokens=2000
+        max_tokens=1000
     )
     last_message = response['choices'][0]['message']['content']
     return last_message.strip()
     
 # Основная функция
-async def process_books(file_path1, file_path2, bot_token, channel_id):
+async def process_book(file_path, bot_token, channel_id):
     try:
-        # Чтение и разбиение первой книги
-        with open(file_path1, 'r', encoding='utf-8') as file:
-            book_text1 = file.read()
-        chapters1 = split_book_into_parts(book_text1)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            book_text = file.read()
 
-        # Чтение и разбиение второй книги
-        with open(file_path2, 'r', encoding='utf-8') as file:
-            book_text2 = file.read()
-        chapters2 = split_book_into_parts(book_text2)
+        chapters = split_book_into_parts(book_text)
 
-        # Отправка начального сообщения
-        start_message = "Бот для анализа книг и отправки пересказов запущен!"
+        # Отправка сообщения о начале работы бота
+        start_message = "Бот для анализа книги и отправки пересказов запущен!"
         await send_message_to_telegram_channel(start_message, bot_token, channel_id)
 
         # Расписание отправки сообщений
-        schedule = [(9, 00), (11,00), (15, 30), (17, 00)]  # (час, минута)
+        schedule = [(9, 0), (15, 30), (17, 00)]  # (час, минута)
         schedule_index = 0
 
-        # Чередование между книгами
-        for (chapter_number1, chapter_text1), (chapter_number2, chapter_text2) in zip(enumerate(chapters1, start=1), enumerate(chapters2, start=1)):
-            # Обработка главы из первой книги
-            trimmed_text1 = trim_text_to_tokens(f"Глава {chapter_number1}\n{chapter_text1}")
-            summary1 = generate_summary(trimmed_text1)
-            await send_message_to_telegram_channel(summary1, bot_token, channel_id)
-            await asyncio.sleep(time_until_next_message(*schedule[schedule_index]))
-            schedule_index = (schedule_index + 1) % len(schedule)
+        for chapter_number, chapter_text in enumerate(chapters, start=1):
+            trimmed_text = trim_text_to_tokens(f"Глава {chapter_number}\n{chapter_text}")
+            summary = generate_summary(trimmed_text)
+            await send_message_to_telegram_channel(summary, bot_token, channel_id)
 
-            # Обработка главы из второй книги
-            trimmed_text2 = trim_text_to_tokens(f"Глава {chapter_number2}\n{chapter_text2}")
-            summary2 = generate_summary(trimmed_text2)
-            await send_message_to_telegram_channel(summary2, bot_token, channel_id)
+            # Ждать до следующего времени в расписании
             await asyncio.sleep(time_until_next_message(*schedule[schedule_index]))
             schedule_index = (schedule_index + 1) % len(schedule)
 
@@ -106,9 +90,8 @@ async def process_books(file_path1, file_path2, bot_token, channel_id):
 
 
 # Пример использования
-file_path1 = "745514.txt"
-file_path2 = "book-bot.txt"
+file_path = "book-bot.txt"  # Используйте относительный путь
 bot_token = '6786746440:AAF2yGdkXhWdnPRzkYZDz1-gweckuTUp-ss'
 channel_id = '@rheniumbooks'
 
-asyncio.run(process_books(file_path1, file_path2, bot_token, channel_id))
+asyncio.run(process_book(file_path, bot_token, channel_id))
